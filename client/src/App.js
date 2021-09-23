faa6c8f82eba912e22c77d7a24af3a1e1e9d38cb
mrrobot11-11 <lanzs@gmatc.matc.edu>
2021-09-22 20:04:07 -0500

    Client Set Up

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component { // Class imports Component from React
+render() {
+    return (
+    <div className="App">
+      <header className="App-header">
+        Good Things
+      </header>
+    </div>
+  );
+}
+}
+
+export default App;
